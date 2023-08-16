73d4e772d14ac8074fd73dbb118f1cb17b9bab5c
변재영 <jybyon1@innodep.com>
2023-08-16 19:09:53 +0900

    [클린코드 5기 변재영]계산기 미션
    *feature: 계산기 기능 구현
    *test: 기능 요구사항에 맞는 테스트 코드 작성

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('hello world2')
+export const ERROR_MESSAGE = Object.freeze({
+  MAX_NUM: "숫자는 한번에 최대 3자리 수까지만 다룰 수 있다.",
+});
+
+class Calculator {
+  validation(a, b) {
+    if (a > 999) {
+      throw new Error(ERROR_MESSAGE.MAX_NUM);
+    }
+    if (b > 999) {
+      throw new Error(ERROR_MESSAGE.MAX_NUM);
+    }
+  }
+
+  sum(a, b) {
+    this.validation(a, b);
+    return Math.floor(a + b);
+  }
+
+  minus(a, b) {
+    this.validation(a, b);
+    return Math.floor(a - b);
+  }
+
+  subtract(a, b) {
+    this.validation(a, b);
+    return Math.floor(a * b);
+  }
+
+  devide(a, b) {
+    this.validation(a, b);
+    return Math.floor(a / b);
+  }
+}
+
+export default Calculator;
